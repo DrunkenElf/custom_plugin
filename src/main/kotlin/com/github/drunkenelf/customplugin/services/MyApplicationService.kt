0bf9b60a533fc7beaaf5772083029d65c939bbad
GitHub Action <action@github.com>
2020-11-13 04:50:20 +0000

    Template cleanup

--- src/main/kotlin/com/github/drunkenelf/customplugin/services/MyApplicationService.kt
+++ src/main/kotlin/com/github/drunkenelf/customplugin/services/MyApplicationService.kt
@@ -1,6 +1,6 @@
-package org.jetbrains.plugins.template.services
+package com.github.drunkenelf.customplugin.services
 
-import org.jetbrains.plugins.template.MyBundle
+import com.github.drunkenelf.customplugin.MyBundle
 
 class MyApplicationService {
 
